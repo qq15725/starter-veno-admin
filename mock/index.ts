2cfae27f4b3f2dc9d7e17262da383a4fb9dc8071
你手捧鲜花 <157215725@qq.com>
2022-01-23 23:14:40 +0800

    fix: mock 配置

--- mock/index.ts
+++ mock/index.ts
@@ -1,7 +1,7 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
 
 // 检索出所有子 mock 文件
-const mocks = Object.values(import.meta.globEager('./**/*.ts')).map(i => i.default)
+const mocks = Object.values(import.meta.globEager('./**/*.ts')).flatMap(i => i.default)
 
 // 设置生产环境数据模型
 export function setupProdMockServer () {
