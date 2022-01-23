d1a2ede70412834fc2fc57a15d894e8af33ac4d2
你手捧鲜花 <157215725@qq.com>
2022-01-23 13:05:11 +0800

    refactor: 基础库 sider 重命名成 drawer

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
   <ve-app style="min-height: 100vh;">
     <!-- 左侧边栏 -->
-    <ve-sider v-model="leftSiderOpened" theme="dark" width="250">
-      <ve-header-title class="text-white">Veno Admin</ve-header-title>
+    <ve-drawer v-model="leftSiderOpened" theme="dark" width="220">
+      <ve-header-title class="mt-3 text-center">Veno Admin</ve-header-title>
       <ve-list nav density="compact" :items="menus" />
-    </ve-sider>
+    </ve-drawer>
 
     <!-- 右侧边栏 -->
-    <ve-sider v-model="rightSiderOpened" width="200" side="right" temporary />
+    <ve-drawer v-model="rightSiderOpened" width="200" anchor="right" temporary />
 
     <!-- 头部 -->
     <ve-header border>
@@ -26,7 +26,7 @@
               v-bind="props"
               href="https://github.com/qq15725/veno-admin"
               target="_blank"
-              icon="svg:M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z"
+              icon="mdi-github"
               variant="text"
               class="ml-3"
           />
@@ -77,8 +77,7 @@ export default defineComponent({
             { title: '工作台', to: '/dashboard/workplace' },
           ]
         },
-        {
-          prependIcon: '$data', title: '数据表格', to: '/table' },
+        { prependIcon: '$data', title: '数据表格', to: '/table' },
       ])
     }
   }
