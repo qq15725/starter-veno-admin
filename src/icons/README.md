7fba413cc802239115bf036df8f44f830c49d0d8
你手捧鲜花 <157215725@qq.com>
2022-03-22 22:33:26 +0800

    docs: fix

--- src/icons/README.md
+++ src/icons/README.md
@@ -1,3 +1,3 @@
 ## 自动注册的图标
 
-查看 [`@veno-ui/vite-plugin-icons`](https://github.com/qq15725/veno-ui/blob/master/packages/vite-plugin-icons) 了解更多详情。
+查看 [vite-plugin-iconify](http://github.com/qq15725/vite-plugin-iconify) 了解更多详情。
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,7 +2,6 @@
 import icons from '~icons'
 import dashboard from '~icons/tabler/dashboard'
 import table from '~icons/tabler/table'
-// import about from '~icons/tabler/question-mark'
 import about from '~icons/ph/question'
 
 export default {
